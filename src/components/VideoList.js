6d8e00bdfae5bb573fca4e9fd55d1df9a7759e49
Sanjeet Uday Jha <sanjeetj@cybage.com>
2023-05-19 18:01:49 +0530

    Added Css

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,23 +6,26 @@ function VideoList({ dispatch, editVideo }) {
   const videos = useVideos();
   return (
     <>
-      {videos.map((video) => (
-        <Video
-          key={video.id}
-          id={video.id}
-          title={video.title}
-          channel={video.channel}
-          views={video.views}
-          time={video.time}
-          verified={video.verified}
-          editVideo={editVideo}
-        >
-          <PlayButton
-            onPlay={() => console.log("play")}
-            onPause={() => console.log("pause")}
-          ></PlayButton>
-        </Video>
-      ))}
+      <div className="flex-container">
+        {videos.map((video) => (
+          <Video
+            key={video.id}
+            id={video.id}
+            title={video.title}
+            channel={video.channel}
+            views={video.views}
+            time={video.time}
+            verified={video.verified}
+            editVideo={editVideo}
+          >
+            <PlayButton
+              onPlay={() => console.log("play")}
+              onPause={() => console.log("pause")}
+            ></PlayButton>
+          </Video>
+        ))}
+      </div>
+
     </>
   );
 }
